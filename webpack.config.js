f0239fdc3c0a6f75db798349b3d9917aef5f260c
yk <yk@126.com>
2018-08-08 23:26:30 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+let path = require('path');
+
+module.export = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
